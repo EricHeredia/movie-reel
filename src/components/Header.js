e604867d0e1df9ec9dc40bcdb88f1b6305d40f3c
Eric Heredia <ericdavidheredia@gmail.com>
2019-08-05 11:56:40 -0500

    fixed startCycle display

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import './Header.scss';
 
 const Header = () => {
   return (
-    <div id="header">
-      <img src="images/logo.png" height="15" width="15" alt="Company Logo" />
-      <p>Header</p>
+    <div>
+      <ul className="nav-bar">
+        <li className="logo"><img src="images/logo.png" alt="Company Logo" /></li>
+        <li><a href="top">DISCOVER</a></li>
+        <li><a href="top">MOVIES</a></li>
+        <li><a href="top">TV SHOWS</a></li>
+      </ul>
     </div>
   )
 }

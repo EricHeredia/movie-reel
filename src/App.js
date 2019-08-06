ec6d1ae4ef37bd78d6ab68e7c52cc534a194629d
Eric Heredia <ericdavidheredia@gmail.com>
2019-08-06 18:53:54 -0500

    added Main.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Main from './containers/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      
+      <Main />
 
       <Footer />
     </div>
---
+++ src/containers/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <p>Main page content</p>
+    </div>
+  )
+}
+
+export default Main;

faa4486ab11abf1aad08ee0f6577a5d230f73d22
Eric Heredia <ericdavidheredia@gmail.com>
2019-08-06 00:28:54 -0500

    working on navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,10 @@ const Header = () => {
     <div>
       <ul className="nav-bar">
         <li className="logo"><img src="images/logo.png" alt="Company Logo" /></li>
-        <li><a href="top">DISCOVER</a></li>
-        <li><a href="top">MOVIES</a></li>
-        <li><a href="top">TV SHOWS</a></li>
+        <li className="nav-link"><a href="top">DISCOVER</a></li>
+        <li className="nav-link"><a href="top">MOVIES</a></li>
+        <li className="nav-link"><a href="top">TV SHOWS</a></li>
+        <li className="nav-link"><a href="top">PEOPLE</a></li>
       </ul>
     </div>
   )
